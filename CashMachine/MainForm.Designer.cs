a23be9a62ab49c89588bed9f78b1c0650db7cbde
Hugo Morales <hugo.camargo87@gmail.com>
2019-02-19 11:27:35 -0300

    Simple GUI

---
+++ CashMachine/MainForm.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace CashMachine
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbxAmountWithdraw = new System.Windows.Forms.TextBox();
+            this.btnWithdraw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tbxAmountWithdraw
+            // 
+            this.tbxAmountWithdraw.Location = new System.Drawing.Point(144, 181);
+            this.tbxAmountWithdraw.Name = "tbxAmountWithdraw";
+            this.tbxAmountWithdraw.Size = new System.Drawing.Size(100, 20);
+            this.tbxAmountWithdraw.TabIndex = 0;
+            // 
+            // btnWithdraw
+            // 
+            this.btnWithdraw.Location = new System.Drawing.Point(162, 217);
+            this.btnWithdraw.Name = "btnWithdraw";
+            this.btnWithdraw.Size = new System.Drawing.Size(75, 23);
+            this.btnWithdraw.TabIndex = 1;
+            this.btnWithdraw.Text = "Sacar";
+            this.btnWithdraw.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(401, 450);
+            this.Controls.Add(this.btnWithdraw);
+            this.Controls.Add(this.tbxAmountWithdraw);
+            this.Name = "MainForm";
+            this.Text = "Cash Machine";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbxAmountWithdraw;
+        private System.Windows.Forms.Button btnWithdraw;
+    }
+}
+
